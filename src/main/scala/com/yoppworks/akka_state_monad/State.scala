b70ad9481cc890db8bd6882d0d857b40d2268bee
Francis Toth <francis.toth@yoppworks.com>
2019-03-30 22:51:36 -0400

    Cleaned up State monad

--- src/main/scala/com/yoppworks/akka_state_monad/State.scala
+++ src/main/scala/com/yoppworks/akka_state_monad/State.scala
@@ -4,19 +4,19 @@ package com.yoppworks.akka_state_monad
 object State {
   type State[S, A] = S => (A, S)
 
-  def apply[S, A](r: S => (A, S)): State[S, A] = (s: S) => r(s)
+  def apply[S, A](r: State[S, A]): State[S, A] = (s: S) => r(s)
 
-  def map[S, A, B](sa: State[S, A])(f: A => B): State[S, B] = state => {
-    val (a, newState) = sa(state)
+  def map[S, A, B](s: State[S, A])(f: A => B): State[S, B] = state => {
+    val (a, newState) = s(state)
     (f(a), newState)
   }
 
-  def flatMap[S, A, B](sa: State[S, A])(f: A => State[S, B]): State[S, B] = state => {
-    val (a, newState) = sa(state)
+  def flatMap[S, A, B](s: State[S, A])(f: A => State[S, B]): State[S, B] = state => {
+    val (a, newState) = s(state)
     f(a)(newState)
   }
 
-  implicit class StateOps[S, A](s: S => (A, S)) {
+  implicit class StateOps[S, A](s: State[S, A]) {
     def map[B](f: A => B): State[S, B] = State.map(s)(f)
     def flatMap[B](f: A => State[S, B]): State[S, B] = State.flatMap(s)(f)
   }
